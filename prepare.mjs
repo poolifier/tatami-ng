3cecb773a2f860645d1146ca0ff4ecb73143fde1
Jérôme Benoit <jerome.benoit@sap.com>
2024-04-03 18:11:48 +0200

    build: add code quality checks at commit
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

---
+++ prepare.mjs
@@ -0,0 +1,8 @@
+import { env } from 'node:process';
+
+const isCIEnvironment = env.CI != null;
+if (isCIEnvironment === false) {
+  import('husky')
+    .then(husky => console.warn(husky.default()))
+    .catch(console.error);
+}
