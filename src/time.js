bc2f6050b28a8a1b1af6f92d617b515d744efe6c
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-09-30 11:35:25 +0200

    fix: do not round timestamping
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- src/time.js
+++ src/time.js
@@ -7,28 +7,28 @@ export const now = (() => {
         const now = performance.now.bind(performance)
         now()
 
-        return () => Math.round(1e6 * now())
+        return () => 1e6 * now()
       } catch {
-        return () => Math.round(1e6 * Date.now())
+        return () => 1e6 * Date.now()
       }
     },
     browser: () => {
       try {
         $.agent.monotonicNow()
 
-        return () => Math.round(1e6 * $.agent.monotonicNow())
+        return () => 1e6 * $.agent.monotonicNow()
       } catch {}
 
       try {
         $262.agent.monotonicNow()
 
-        return () => Math.round(1e6 * $262.agent.monotonicNow())
+        return () => 1e6 * $262.agent.monotonicNow()
       } catch {}
 
-      return () => Math.round(1e6 * performance.now())
+      return () => 1e6 * performance.now()
     },
     node: () => () => Number(process.hrtime.bigint()),
-    deno: () => () => Math.round(1e6 * performance.now()),
+    deno: () => () => 1e6 * performance.now(),
     bun: () => Bun.nanoseconds,
   }[runtime]()
 })()
