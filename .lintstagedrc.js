7edf0d47c4e06f880e0dc0b2b28c9cbbd11fbed7
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-11-16 13:51:44 +0100

    chore: refine lint-staged configuration
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,6 @@
 export default {
-  '**/*.{ts,tsx,js,jsx,cjs,mjs,json,jsonc}': ['biome check --apply'],
+  '**/*.{ts,tsx,js,jsx,cjs,mjs,json,jsonc}': [
+    'biome check --no-errors-on-unmatched --apply',
+  ],
   '**/*.{md,yml,yaml}': ['prettier --cache --write'],
 }
